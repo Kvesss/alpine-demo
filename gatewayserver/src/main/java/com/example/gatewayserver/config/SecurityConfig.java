4a298859209a4acbc299b525162d4789aef82ecb
david.kvesic <david.kvesic@ibmix.hr>
2025-02-18 15:34:46 +0100

    implemented security authorization config, added keycloak docker container

--- gatewayserver/src/main/java/com/example/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/example/gatewayserver/config/SecurityConfig.java
@@ -2,11 +2,17 @@ package com.example.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -18,8 +24,17 @@ public class SecurityConfig {
                                                           .pathMatchers("alpine/accounts/**").authenticated()
                                                           .pathMatchers("alpine/cards/**").authenticated()
                                                           .pathMatchers("alpine/loans/**").authenticated())
-                 .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()));
+                 .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(jwt -> jwt
+                                                                                   .jwtAuthenticationConverter(this.grantedAuthoritiesExtractor())));
         return serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
                                  .build();
+    }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        final JwtAuthenticationConverter jwtAuthenticationConverter =
+          new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter
+                                    (new KeycloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
     }
 }
